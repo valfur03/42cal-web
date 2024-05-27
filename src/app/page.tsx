3ff6d7556d68c186fe08c97a27dac71598a6c3c1
vfurmane <vfurmane@student.42.fr>
2024-05-27 21:03:21 +0200

    feat: add a link to the GitHub repository

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import { TypographyH1, TypographyLead } from "@/components/ui/typography";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { GITHUB_PROJECT_URL } from "@/common/constants/meta";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8 md:p-24">
       <TypographyH1>42cal</TypographyH1>
-      <TypographyLead className="mt-2 md:mt-6 text-center">
+      <TypographyLead className="my-2 md:my-6 text-center">
         An iCal API to see the 42 events in a calendar.
       </TypographyLead>
+      <Button asChild>
+        <Link href={GITHUB_PROJECT_URL}>
+          <GitHubLogoIcon className="h-5 w-5 mr-2" /> GitHub repository
+        </Link>
+      </Button>
     </main>
   );
 }
---
+++ src/common/constants/meta.ts
@@ -0,0 +1,1 @@
+export const GITHUB_PROJECT_URL = "https://github.com/vfurmane/42cal/";
