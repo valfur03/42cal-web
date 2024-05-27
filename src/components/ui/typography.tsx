27b411c875c39d2573da3f48418969f391122863
vfurmane <vfurmane@student.42.fr>
2024-05-27 21:33:19 +0200

    feat: add shadcn TypographyH2

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -11,6 +11,14 @@ export function TypographyH1({ children, className }: TypographyProps) {
   );
 }
 
+export function TypographyH2({ children, className }: TypographyProps) {
+  return (
+    <h2 className={cn("scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0", className)}>
+      {children}
+    </h2>
+  );
+}
+
 export function TypographyLead({ children, className }: TypographyProps) {
   return <p className={cn("text-xl text-muted-foreground", className)}>{children}</p>;
 }
