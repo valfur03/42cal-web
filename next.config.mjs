6fee4a695da228d545de692a58b1abd6fc258d46
vfurmane <vfurmane@student.42.fr>
2024-05-29 21:14:55 +0200

    feat: remove redirect from /events to API

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async redirects() {
-    return [{
-      source: '/events',
-      destination: `${process.env.API_BASE_URL}/events`,
-      permanent: true,
-    }]
-  }
 };
 
 export default nextConfig;
