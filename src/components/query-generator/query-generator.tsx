cd294d21907871d0e72a4f3d60c5b37272d6f331
vfurmane <vfurmane@student.42.fr>
2024-05-29 21:13:51 +0200

    feat: add instructions on how to use the tool

--- src/components/query-generator/query-generator.tsx
+++ src/components/query-generator/query-generator.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TypographyH2 } from "@/components/ui/typography";
+import { TypographyH2, TypographyOList, TypographyP } from "@/components/ui/typography";
 import { QueryGeneratorForm } from "@/components/query-generator/query-generator-form";
 import { ClipboardBlock } from "@/components/ui/clipboard-block";
 import { useQueryGenerator } from "@/components/query-generator/shared/hooks/use-query-generator";
@@ -19,6 +19,13 @@ export function QueryGenerator({ campuses }: QueryGeneratorProps) {
   return (
     <div className="w-full mt-20">
       <TypographyH2>Configure your own route</TypographyH2>
+      <TypographyP>Follow these simple steps:</TypographyP>
+      <TypographyOList>
+        <li>Choose a campus</li>
+        <li>Copy the URL</li>
+        <li>Paste it in your calendar app</li>
+      </TypographyOList>
+      <TypographyP>And your done! ✨</TypographyP>
       <QueryGeneratorForm
         basicToken={basicToken}
         setBasicToken={setBasicToken}
